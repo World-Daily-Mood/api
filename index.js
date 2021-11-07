5c92a932143ace50735409dbf35d43f2a0a9d0a1
KristN <kristian.kunc@gmail.com>
2021-11-07 12:58:04 +0100

    added working key check

--- index.js
+++ index.js
@@ -9,31 +9,77 @@ let mysql_raw = fs.readFileSync("mysql/config.json");
 let mysql_config = JSON.parse(mysql_raw);
 const keys = new Keys(mysql_config["host"], mysql_config["username"], mysql_config["password"], mysql_config["database"]);
 
-function check_api_key(req, res) {
+function check_key_presence(req, res) {
     const api_key = req.get("Authentication");
 
     if (api_key == null) {
-        res.status(401).send({"message": "401: Unauthorized"});
-        return false;
+        res.status(403).send({"message": "403: Forbidden"});
+        return false
+    } else {
+        return true;
     }
-    
-    keys.check(api_key)
+}
+
+function api_key_is_valid(req, res) {
+    const api_key = req.get("Authentication");
+
+    if (check_key_presence(req, res)) {
+        return new Promise(function(resolve, reject) {
+            keys.check(api_key).then(result => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    res.status(403).send({"message": "403: Forbidden"});
+                    resolve(result);
+                }
+            });
+        }
+    )} else {
+        return new Promise(function(resolve, reject) {
+            resolve(false);
+        }); 
+    }
 }
 
 app.get("/", (req, res) => {
     res.send("Api root");
 });
 
-app.get("/keycheck", (req, res) => {
-    if (check_api_key(req, res) == false) {
-        return;
+app.get("/key/check", (req, res) => {
+    const api_key = req.get("Authentication");
+
+    api_key_is_valid(req, res).then(result => {
+        if (result) {
+            res.send({data: api_key});
+        }
+    });
+});
+
+
+
+/*
+    if (check_key_presence(req, res) == true) {
+        keys.check(api_key, function(err, result) {
+        if (result == true) {
+            res.send({data: api_key});
+
+        } else {
+            res.status(403).send({"message": "403: Forbidden"});
+        }
+        });
     }
 
-    res.send({data: "success"});
+
+});
+*/
+
+app.get("/key/generate", (req, res) => {
+    var api_keys = keys.generate(2);
+    console.log(api_keys);
+
+    res.send(api_keys);
 });
 
-app.get("/test", (req, res) => {
-    keys.test(keys)
-});
+//console.log(app._router.stack);
 
 app.listen(port, () => console.log("Server started on " + port));
--- mysql/keys.js
+++ mysql/keys.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql");
+const { createHash } = require('crypto');
+const { randomBytes } = require("crypto");
 
 class Keys {
     constructor(host, user, password, database) {
@@ -17,22 +19,48 @@ class Keys {
         });
     }
 
-    disconnect(connection){
-        connection.end()
+    check(key) {
+        const connection = this.connect();
+        const api_key = createHash('sha256').update(key).digest('hex');
+
+        return new Promise(function(resolve, reject) {
+            connection.query("SELECT user_id FROM api_keys WHERE api_key = ?", [api_key], function(err, rows) {
+
+                if (err) throw err;
+                if (rows.length > 0) {
+                    resolve(true);
+                }
+                else {
+                    resolve(false);
+                }
+
+            });
+
+            connection.end();
+        });
     }
 
-    check(key){
+
+    generate(user_id) {
+        const key = randomBytes(16).toString("hex");
+        const hashed_key = createHash("sha256").update(key).digest("hex");
+      
         var connection = this.connect()
-
-        var query = connection.query(`SELECT user_id from api_keys where api_key = '${key}';`, function(err, result){
+        connection.query(`SELECT api_key from api_keys where api_key = '${key}';`, function(err, result){
             if(err) throw err;
-            
-            console.log(result[0]["user_id"]);
 
+            if (result == null){
+                generate();
+            }
+            else{
+                connection.query(`INSERT INTO api_keys (user_id, api_key) VALUES (${user_id}, "${hashed_key}");`, function(err, result){
+                    if(err) throw err;
+                });
+            }
         });
 
-        this.disconnect(connection)
-
+        connection.end();
+        return {key, hashed_key};
     }
 }
 
